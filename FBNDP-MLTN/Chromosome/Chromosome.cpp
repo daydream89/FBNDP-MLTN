a80f30885865179ca7e3e249a3c6f108fa68b195
minihami <minihami@naver.com>
2021-01-16 13:24:39 +0900

    Initial Population
    초기 집단 생성에 필요한 함수들 추가. 몇몇 버그가 보여 조금 더 수정이 필요함

--- FBNDP-MLTN/Chromosome/Chromosome.cpp
+++ FBNDP-MLTN/Chromosome/Chromosome.cpp
@@ -1,18 +1,108 @@
 #include "Chromosome.h"
+#include "../util/Utils.h"
 #include <random>
+#include <map>
 
-Chromosome::Chromosome(vector<NodeData> RailNode, vector<NodeData> BusNode)
+Chromosome::Chromosome(vector<NodeData> RailNode, vector<NodeData> BusNode):bAllRailStationHaveRoute(false),BusRouteNum(0)
 {
-	bAllRailStationHaveRoute = false;
 	this->RailNode.assign(RailNode.begin(), RailNode.end());
 	CopiedBusNode.assign(BusNode.begin(), BusNode.end());
-	RailStationSelected.resize(RailNode.size());
+	for (auto CheckRailNode : RailNode)
+		RailStationSelected[CheckRailNode.Num] = false;
 
+#if 0
 	while (CopiedBusNode.size() > 0) {
-		NodeData SelectedNode = SelectRailNode();
+		NodeData SelectedRailNode = SelectRailNode();
 		/* TODO */
-		//GenRouletteWheelPercent(SelectedNode);
+		SelectedBusNodeData SelectedBus = SelectBusNode(SelectedRailNode);
+		if (!bAllRailStationHaveRoute)
+		{
+			if (bAllRailStationHaveRoute == false)
+			{
+				bool StationCheckFlag = true;
+				if (RailStationSelected.find(SelectedRailNode.Num)->second == false)
+					RailStationSelected[SelectedRailNode.Num] = true;
+				for (auto StationSelectedCheck : RailStationSelected)
+				{
+					if (StationSelectedCheck.second == false)
+					{
+						StationCheckFlag = false;
+						break;
+					}
+				}
+				if (StationCheckFlag)
+					bAllRailStationHaveRoute = true;
+			}
+			++BusRouteNum; //k = k+1
+			ShortestPathData RoutePathData;
+			RoutePathData.Path.assign(SelectedBus.BusRouteData.Path.begin(), SelectedBus.BusRouteData.Path.end());
+			RoutePathData.Cost = SelectedBus.BusRouteData.Cost;
+			RouteDataList.emplace_back(RoutePathData);
+		}
+		else
+		{
+			/*TODO : Find Shortest Path Routes...*/
+			ShortestPathData FoundedShortestRoute;
+			ShortestPathData ExistRoute;
+			for (auto RouteDataIter : RouteDataList)
+			{
+				float MinRouteLength = INFINITY;
+				printf("Route Node Data Num: %d\n", RouteDataIter.Path.end()->Num);
+				if (RouteDataIter.Path.end()->Num == SelectedRailNode.Num) //Current Selected Rail Node Routes...
+				{
+					float RouteLength = 0;
+					vector<NodeData> InputGraph;
+					vector<ShortestPathData> ShortestRoute;
+					InputGraph.assign(CopiedBusNode.begin(), CopiedBusNode.end());
+					PathFinderData ShortestPathData(InputGraph, SelectedBus.SelectedBusNode.Num, RouteDataIter.Path.begin()->Num, EPathFinderCostType::Length, 1);
+					Util::PathFinder::FindShortestPath(ShortestPathData, ShortestRoute);
+					RouteLength = ShortestRoute.at(0).Cost;
+					if (RouteLength < MinRouteLength)
+					{
+						MinRouteLength = RouteLength;
+						FoundedShortestRoute.Path.clear();
+						FoundedShortestRoute.Path.assign(ShortestRoute.begin()->Path.begin(), ShortestRoute.begin()->Path.end());
+						FoundedShortestRoute.Cost = RouteLength + RouteDataIter.Cost;
+						ExistRoute = RouteDataIter;
+					}
+				}
+			}
+			/*TODO: SelectedBus.BusRouteData.Cost vs Shortest Cost include exist route */
+			if (FoundedShortestRoute.Cost < SelectedBus.BusRouteData.Cost) /* Find Lesat Cost Path */
+			{
+				for (auto RouteIter : RouteDataList)
+				{
+					if (RouteIter.Path.begin()->Num == ExistRoute.Path.begin()->Num)
+					{
+						RouteIter.Path.insert(RouteIter.Path.begin(), FoundedShortestRoute.Path.begin(), FoundedShortestRoute.Path.end());
+					}
+				}
+			}
+			else
+			{
+				++BusRouteNum; //k = k+1
+				ShortestPathData RoutePathData;
+				RoutePathData.Path.assign(SelectedBus.BusRouteData.Path.begin(), SelectedBus.BusRouteData.Path.end());
+				RoutePathData.Cost = SelectedBus.BusRouteData.Cost;
+				RouteDataList.emplace_back(RoutePathData);
+			}
+			
+		}
+
+		for (vector<NodeData>::const_iterator NodeIter = CopiedBusNode.begin(); NodeIter != CopiedBusNode.end();) //B' = \i
+		{
+			if (NodeIter->Num == SelectedBus.SelectedBusNode.Num)
+			{
+				CopiedBusNode.erase(NodeIter);
+				break;
+			}
+			else
+			{
+				++NodeIter;
+			}
+		}
 	}
+#endif
 
 }
 NodeData Chromosome::SelectRailNode()
@@ -21,29 +111,84 @@ NodeData Chromosome::SelectRailNode()
 	mt19937 gen(rd());
 	uniform_int_distribution<int> dis(0, RailNode.size()-1);
 	uint32_t RandomNum = dis(gen);
-	if (bAllRailStationHaveRoute == false) {
+	/*
+	if (bAllRailStationHaveRoute == false) 
+	{
+		bool StationCheckFlag = true;
 		if (RailStationSelected.at(RandomNum) == false)
 			RailStationSelected.at(RandomNum) = true;
 		for (auto StationSelectedCheck : RailStationSelected)
 		{
 			if (StationSelectedCheck == false)
+			{
+				StationCheckFlag = false;
 				break;
-			else
-				bAllRailStationHaveRoute = true;
+			}
 		}
+		if (StationCheckFlag)
+			bAllRailStationHaveRoute = true;
 	}
+	*/
 	return RailNode.at(RandomNum);
 }
 
-#if 0
-vector<NodeSelectPercent> Chromosome::GenRouletteWheelPercent(const NodeData& SelectedRailNode)
+#if 1
+SelectedBusNodeData Chromosome::SelectBusNode(const NodeData& SelectedRailNode)
 {
 	/* TODO */
 
-}
+	map<uint32_t, ShortestPathData> FoundBusRouteMap;
+	float TotalRouteLength = 0;
+	for (auto BusNodeIter : CopiedBusNode)
+	{
+		vector<NodeData> InputGraph;
+		vector<ShortestPathData> ShortestRoute;
+		InputGraph.assign(CopiedBusNode.begin(), CopiedBusNode.end());
+		InputGraph.emplace_back(SelectedRailNode);
+		PathFinderData ShortestPathData(InputGraph, BusNodeIter.Num, SelectedRailNode.Num, EPathFinderCostType::Length, 1);
+		Util::PathFinder::FindShortestPath(ShortestPathData, ShortestRoute);
+		if (ShortestRoute.begin()->Cost == INFINITY)
+		{
+			printf("INF Route\n");
+			continue;
+		}
+		TotalRouteLength += ShortestRoute.at(0).Cost;
+		FoundBusRouteMap.insert(make_pair(BusNodeIter.Num, ShortestRoute.at(0)));
+		printf("From %d to %d, Shortest Path Length : %f\n", BusNodeIter.Num, SelectedRailNode.Num, ShortestRoute.at(0).Cost);
+	}
 
-NodeData SelectBusNode(const vector<NodeSelectPercent>& NodePercent)
-{
-	/* TODO */
+	random_device rd;
+	mt19937 gen(rd());
+	uniform_real_distribution<float> dis(0.0f, 1.0f);
+	float RandomNum = dis(gen);
+
+	float CumulativeProbability = 0.0f;
+	uint32_t SelectedBusNodeNum;
+	for (auto BusMapIter : FoundBusRouteMap)
+	{
+		float LowRange = CumulativeProbability;
+		float HighRange = CumulativeProbability + (BusMapIter.second.Cost/TotalRouteLength);
+		if  (RandomNum >= LowRange && RandomNum <HighRange)
+		{
+			SelectedBusNodeNum = BusMapIter.first;
+			break;
+		}
+		CumulativeProbability += (BusMapIter.second.Cost/TotalRouteLength);
+	}
+
+	SelectedBusNodeData SelectedData;
+	for (auto BusNodeIter : CopiedBusNode)
+	{
+		if (BusNodeIter.Num == SelectedBusNodeNum) 
+		{
+			SelectedData.SelectedBusNode = BusNodeIter;
+			break;
+		}
+	}
+
+	SelectedData.BusRouteData = FoundBusRouteMap.find(SelectedBusNodeNum)->second;
+	printf("Selected Bus Node Num: %d\n", SelectedBusNodeNum);
+
+	return SelectedData;
 }
 #endif
--- FBNDP-MLTN/Chromosome/Chromosome.h
+++ FBNDP-MLTN/Chromosome/Chromosome.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "../Data/DataTypes.h"
+#include <map>
 
 struct NodeSelectPercent
 {
@@ -11,18 +12,22 @@ struct BusRouteData
 	uint32_t BusRouteNum;	//flowchart k
 	vector<NodeData> RouteNodes;
 };
+struct SelectedBusNodeData
+{
+	NodeData SelectedBusNode;
+	ShortestPathData BusRouteData;
+};
 
 class Chromosome
 {
 public:
 	Chromosome(vector<NodeData> RailNode, vector<NodeData> BusNode);
-	uint32_t GetRouteNum(){ return RouteList.size(); }
+	uint32_t GetRouteNum(){ return RouteDataList.size(); }
 	vector<NodeData> GetChromosome() { return ChromosomeNodeList; }
 	
 private:
 	NodeData SelectRailNode();
-	vector<NodeSelectPercent> GenRouletteWheelPercent(const NodeData& SelectedRailNode);
-	NodeData SelectBusNode(const vector<NodeSelectPercent>& NodePercent);
+	SelectedBusNodeData SelectBusNode(const NodeData& SelectedRailNode);
 
 
 	void CalculateNetworkCost();
@@ -32,11 +37,12 @@ private:
 	vector<NodeData> BusNode;	//flowchart B
 	vector<NodeData> RailNode;	//Rail Station Nodes
 
-	vector<bool> RailStationSelected;
+	map<uint32_t, bool> RailStationSelected;
 	bool bAllRailStationHaveRoute;
 
 	vector<NodeData> CopiedBusNode;	//flowchart B'
 
-	vector<vector<NodeData>> RouteList;
+	uint32_t BusRouteNum; //flowchart k
+	vector<ShortestPathData> RouteDataList;
 	vector<NodeData> ChromosomeNodeList;
 };
